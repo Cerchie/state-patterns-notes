a7bcb4358afa21b3f3039a0d1631e779c2f408c1
Lucia Cerchie <luciacerchie@gmail.com>
2020-12-02 13:27:25 -0700

    removing example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import SimpleCounter from './SimpleCounter'
+import NumbersList from './NumbersList'
 function App() {
   return (
     <div className="App">
+       <NumbersList />
       <SimpleCounter />
+     
     </div>
   );
 }
--- src/SimpleCounter.js
+++ src/SimpleCounter.js
@@ -3,18 +3,23 @@ import React, {useState} from 'react';
 
 function SimpleCounter() {
     const [num, setNum] = useState(0);
-    const [num2, setNum2] = useState(0);
+    
     console.log("RUNNING AGAIN! NUM IS", num)
 
     const clickUp = () => {
-        setNum(num + 1);
+        setNum(n => n + 1);
+    }
+
+    const clickUpBy2 = () => {
+        setNum(n => n + 1);
+        setNum(n => n + 1);
+    
     }
     return (
         <div>
             <h3>Count: {num} </h3>
             <button onClick={clickUp}>Up</button>
-            <h3>Count2: {num2} </h3>
-            <button onClick={()=> setNum2(num2 +1)}>Up</button>
+            <button onClick={clickUpBy2}>Up By 2</button>
         </div>
     )
 }
